a8ab1a14e6e3192a65c98935ae7afc5f11287e0c
Rahees Ahmed <raheesahmed256@gmail.com>
2024-09-08 13:55:47 +0500

    api url fixed

--- app/api/google/analytics/route.ts
+++ app/api/google/analytics/route.ts
@@ -4,7 +4,7 @@ import { google } from 'googleapis';
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  `${process.env.NEXTAUTH_URL}/api/auth/callback/google-analytics`
+  `${process.env.NEXTAUTH_URL}/api/google/analytics`
 );
 
 export async function GET(req: NextRequest) {
